a0bf6e3a54604dfb40028ad160e3be7755263f22
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-12 19:53:14 -0300

    Add poem file to be used for development

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query_string = &args[1];
     let filename = &args[2];
     println!("querying {} in file {}", query_string, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    println!("Contents: \n{}", contents);
 }
