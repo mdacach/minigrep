af2fcc887127816396ece97d827c688546651bad
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-12 19:49:05 -0300

    Parse commandline arguments query and file

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,8 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query_string = &args[1];
+    let filename = &args[2];
+    println!("querying {} in file {}", query_string, filename);
 }
